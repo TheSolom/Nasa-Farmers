e1597f322541994eea1367efbcf46a8c8e79b4b9
Islam Ashraf <eslam.01212@gmail.com>
2024-10-04 21:21:09 +0300

    refactor: add error handling

--- backend/src/modules/user/api/user.controller.js
+++ backend/src/modules/user/api/user.controller.js
@@ -4,14 +4,20 @@ import HttpStatusCodes from '../../../shared/utils/httpStatusCodes.js';
 export async function getUser(req, res, _next) {
     const { params: { userId } } = req;
 
-    const user = await userService.getUserById(userId);
+    try {
+        const user = await userService.getUserById(userId);
 
-    res.status(HttpStatusCodes.OK).json({
-        id: user._id,
-        fullName: user.fullName,
-        email: user.email,
-        job: user.job,
-        createdAt: user.createdAt,
-        updatedAt: user.updatedAt
-    });
+        res.status(HttpStatusCodes.OK).json({
+            id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            job: user.job,
+            createdAt: user.createdAt,
+            updatedAt: user.updatedAt
+        });
+    } catch (error) {
+        return res.status(HttpStatusCodes.NOT_FOUND).json({
+            message: error.message
+        });
+    }
 }
